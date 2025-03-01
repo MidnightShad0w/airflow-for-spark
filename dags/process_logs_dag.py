7fc340738c3738970a612eb8d8020f40f4611156
MidnightShad0w <samsonchik2406@mail.ru>
2025-03-01 16:37:47 +0300

    minor #1

--- dags/process_logs_dag.py
+++ dags/process_logs_dag.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2025, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
