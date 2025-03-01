6441738acffbe088b537b73655467233701b20aa
MidnightShad0w <samsonchik2406@mail.ru>
2025-03-01 14:31:02 +0300

    init

---
+++ dags/process_logs_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.http_operator import SimpleHttpOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+with DAG(
+    dag_id='process_logs_dag',
+    default_args=default_args,
+    schedule_interval='*/2 * * * *',  # каждые 2 минуты
+    catchup=False
+) as dag:
+
+    process_files = SimpleHttpOperator(
+        task_id='process_csv_files',
+        http_conn_id='spark_processing_conn',
+        endpoint='/spark/api/process',
+        method='POST',
+        headers={"Content-Type": "application/json"},
+        data='{"filePath": "s3a://bucket-spark/fulldata/"}',
+    )
+
+    process_files
